91b5cc5a2da0e1405560edb02942d51b10b1caef
Lauren Lu <myemmamy@Laurens-MacBook-Pro.local>
2026-02-10 15:19:38 -0800

    fix nums=[2,7,11,13] problem

--- leetcode/0001_two_sum/solution.go
+++ leetcode/0001_two_sum/solution.go
@@ -1,23 +1,24 @@
 package main
+
 import "fmt"
 
 func twoSum(nums []int, target int) []int {
-    d := make(map[int]int)
-    for i :=0 ; i<len(nums) ; i++ {
-        m := target - nums[i]
-        val,ok := d[m]
-        if ok {
-            return []int{val,i}
-        } else {
-            d[nums[i]] = i
-        }
-    }
-    return nil
+	d := make(map[int]int)
+	for i := 0; i < len(nums); i++ {
+		m := target - nums[i]
+		val, ok := d[m]
+		if ok {
+			return []int{val, i}
+		} else {
+			d[nums[i]] = i
+		}
+	}
+	return nil
 }
 
 func main() {
-  nums := [2,7,11,13]
-  target := 9
-  a := twoSum(nums,target)
-  fmt.Prinln(a)
+	nums := []int{2, 7, 11, 13}
+	target := 9
+	a := twoSum(nums, target)
+	fmt.Println(a)
 }
